f2bc9cd1331bd3ced3f4adf08c7a7b1a786b0d65
Anthony Rivas <anthony.edward.rivas@gmail.com>
2020-08-17 19:49:19 -0700

    minor fix for card title inn solved

--- 20-State/01-Activities/12-Stu_DynamicContext/Solved/src/components/CardTitleText/index.js
+++ 20-State/01-Activities/12-Stu_DynamicContext/Solved/src/components/CardTitleText/index.js
@@ -3,7 +3,7 @@ import UserContext from "../../utils/userContext";
 
 function CardTitleText() {
   const { user } = useContext(UserContext);
-  return <h2>{user.title}</h2>;
+  return <h2>{user.login}</h2>;
 }
 
 export default CardTitleText;
